af7fcde58ad7ad0b6e0d878b545f32a1a1e173d6
Uthman Mohamed <83053931+1239uth@users.noreply.github.com>
2024-03-31 03:04:00 -0400

    test: add integ test for controller

--- src/test/kotlin/com/example/familytree/FamilyTreeApplicationTests.kt
+++ src/test/kotlin/com/example/familytree/FamilyTreeApplicationTests.kt
@@ -1,13 +1,28 @@
 package com.example.familytree
 
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class FamilyTreeApplicationTests {
+    @Autowired
+    private lateinit var mockMvc: MockMvc
 
     @Test
     fun contextLoads() {
     }
 
+    @Test
+    fun `should return all members`() {
+        mockMvc.perform(get("/members"))
+            .andExpect(status().isOk)
+            .andExpect(content().json("[]"))
+    }
 }
